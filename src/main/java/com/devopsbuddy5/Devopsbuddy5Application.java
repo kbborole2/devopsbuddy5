8e69250176de7fa091b90229a7161ff2f9cdea43
Khetho Borole <kbborole2@gmail.com>
2017-07-10 13:19:35 -0700

    initial commit to Spring boot project

---
+++ src/main/java/com/devopsbuddy5/Devopsbuddy5Application.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopsbuddy5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devopsbuddy5Application.class, args);
+	}
+}
